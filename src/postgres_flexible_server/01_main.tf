ca969d6a49521a588f055a289fb331c5a707c9ac
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2025-04-29 12:17:51 +0200

    fix

--- src/postgres_flexible_server/01_main.tf
+++ src/postgres_flexible_server/01_main.tf
@@ -12,8 +12,8 @@ locals {
 }
 
 
-resource "azurerm_postgresql_flexible_server" "this" {
-
+module "postgresql_flexible_server_idh" {
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v4.git//postgres_flexible_server?ref=v5.4.0"
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
